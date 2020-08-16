5969b2ed0e7629e2ab4dab60ba984ebbf91229b7
Alex <alexfreirespinola@gmail.com>
2020-08-16 03:07:32 -0300

    refactor: home screen to use transactions hook

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,10 @@ import styled from 'styled-components/native';
 
 import PageHeader from '../components/PageHeader';
 import BalanceCard from '../components/BalanceCard';
-import {
-  ITransactionCategoryEnum,
-  ITransactionTypeEnum,
-  ITrasanctionItemProps,
-} from '../components/TransactionItem';
+
 import TransactionList from '../components/TransactionList';
+
+import {useTransactions} from '../providers/TransactionsProvider';
 
 const Container = styled.View`
   padding-top: 10px;
@@ -17,45 +15,18 @@ const Container = styled.View`
 `;
 
 const Home = (props: any) => {
-  const data = [
-    {
-      id: '1',
-      amount: 100.0,
-      title: 'Compra de Comida',
-      category: ITransactionCategoryEnum.FOOD,
-      type: ITransactionTypeEnum.OUTCOME,
-    },
-    {
-      id: '1',
-      amount: 100.0,
-      title: 'Recebimento de dividendos',
-      type: ITransactionTypeEnum.INCOME,
-    },
-    {
-      id: '3',
-      amount: 1500.0,
-      title:
-        'Recebimento de bonificação bonificação bonificação bonificação bonificação',
-      type: ITransactionTypeEnum.INCOME,
-    },
-    {
-      id: '4',
-      amount: 1000.0,
-      title: 'Aluguel',
-      category: ITransactionCategoryEnum.HABITATITON,
-      type: ITransactionTypeEnum.OUTCOME,
-    },
-  ] as ITrasanctionItemProps[];
+  const {transactions} = useTransactions();
+
   return (
     <>
-      <PageHeader label="Home" />
+      <PageHeader label="+Finanças" />
       <Container>
         <BalanceCard
           todayIncome={1000.0}
           todayOutcome={525.3}
           balance={3000.255}
         />
-        <TransactionList title="Últimas transações" data={data} />
+        <TransactionList title="Últimas transações" data={transactions} />
       </Container>
     </>
   );
