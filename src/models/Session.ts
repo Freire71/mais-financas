dda984a531f277680aa09f8377521aea66ad5088
Alex <alexfreirespinola@gmail.com>
2020-08-17 16:49:29 -0300

    feat: add session model

---
+++ src/models/Session.ts
@@ -0,0 +1,35 @@
+import {ObjectId} from 'bson';
+import User from './User';
+
+export interface ICreateSessionData {
+  id?: string;
+  user: User;
+  created_at?: Date;
+  ended_at?: Date;
+}
+
+class Session {
+  public id: string;
+  public owner: User;
+  public created_at: Date;
+  public ended_at: Date | null;
+
+  constructor(data: ICreateSessionData) {
+    this.id = data.id ? data.id : String(new ObjectId());
+    this.owner = data.user;
+    this.created_at = data.created_at ? data.created_at : new Date();
+    this.ended_at = data.ended_at ? data.ended_at : null;
+  }
+  static schema = {
+    name: 'Session',
+    primaryKey: 'id',
+    properties: {
+      id: {type: 'string', indexed: true},
+      created_at: {type: 'date', default: new Date(), indexed: true},
+      ended_at: {type: 'date?', indexed: true},
+      owner: 'User',
+    },
+  };
+}
+
+export default Session;
