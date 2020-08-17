1926a83631c4011f53cbebddf9e22220553bab9d
Alex <alexfreirespinola@gmail.com>
2020-08-17 16:51:33 -0300

    feat: add new schemas to realm

--- src/realm/index.ts
+++ src/realm/index.ts
@@ -1,6 +1,8 @@
 import Realm from 'realm';
 import Transaction from '../models/Transaction';
 import Balance from '../models/Balance';
+import User from '../models/User';
+import Session from '../models/Session';
 
 let app: Realm;
 
@@ -8,11 +10,16 @@ const getRealm = async () => {
   if (!app) {
     try {
       app = await Realm.open({
-        schema: [Transaction.schema, Balance.schema],
+        schema: [
+          Transaction.schema,
+          Balance.schema,
+          User.schema,
+          Session.schema,
+        ],
       });
       console.log('Realm instanciado com sucesso');
     } catch (err) {
-      console.log('Error ao instanciar o Realm');
+      console.log('Error ao instanciar o Realm: ', err);
     }
   }
   return app;
