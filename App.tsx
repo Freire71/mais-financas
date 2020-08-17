5243f1df1ddb11f0867a7d14fb624bcdc29a775f
Alex <alexfreirespinola@gmail.com>
2020-08-17 01:08:54 -0300

    feat: add balance provided import to app root

--- App.tsx
+++ App.tsx
@@ -2,19 +2,24 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 import 'react-native-gesture-handler';
 import {ThemeProvider} from 'styled-components/native';
+import {LogBox} from 'react-native';
+LogBox.ignoreLogs(['componentWillReceiveProps', 'Require cycle']); // warning threw by external libraries
 
 import NavigationContainer from './src/config/Navigator';
 import theme from './src/config/Theme';
 import {TransactionsProvider} from './src/providers/TransactionsProvider';
+import {BalanceProvider} from './src/providers/BalanceProvider';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <TransactionsProvider>
-        <NavigationContainer>
-          <StatusBar barStyle="dark-content" />
-        </NavigationContainer>
-      </TransactionsProvider>
+      <BalanceProvider>
+        <TransactionsProvider>
+          <NavigationContainer>
+            <StatusBar barStyle="dark-content" />
+          </NavigationContainer>
+        </TransactionsProvider>
+      </BalanceProvider>
     </ThemeProvider>
   );
 };
