93527af152fbbe9121e1297876c23951a3d808e1
Alex <alexfreirespinola@gmail.com>
2020-08-17 21:45:18 -0300

    feat: add user logic associated to balance

--- src/providers/BalanceProvider.tsx
+++ src/providers/BalanceProvider.tsx
@@ -1,8 +1,9 @@
 import React, {useEffect, useState, useContext} from 'react';
 import Realm from 'realm';
-import Balance, {ICreateBalanceData} from '../models/Balance';
 
+import Balance, {ICreateBalanceData} from '../models/Balance';
 import getRealm from '../realm';
+import {useAuth} from './AuthProvider';
 
 const BalanceContext = React.createContext<IBalanceProvider | null>(null);
 
@@ -15,29 +16,38 @@ const BalanceProvider = ({children}: {children: React.ReactChild}) => {
   let app: Realm;
   const collectionName = 'Balance';
 
+  const {currentUserId, getUser} = useAuth();
+
   const [currentBalance, setCurrentBalance] = useState(0);
   useEffect(() => {
+    if (!currentUserId) {
+      return setCurrentBalance(0);
+    }
     async function setup() {
       await loadLastBalance();
     }
     setup();
-  });
+  }, [currentUserId]);
 
   const loadLastBalance = async () => {
     app = await getRealm();
+    const user = await getUser();
     const balances = app
       .objects<Balance>(collectionName)
+      .filtered('owner = $0', user)
       .sorted('created_at', true);
     if (!balances[0]) {
-      await createNewBalance({amount: 0});
+      await createNewBalance({amount: 0, owner: user});
     } else {
       setCurrentBalance(balances[0].amount);
     }
   };
 
   const createNewBalance = async (data: ICreateBalanceData) => {
-    const balance = new Balance(data);
     app = await getRealm();
+    const user = await getUser();
+    const balance = new Balance({...data, owner: user});
+
     try {
       app.write(() => {
         app.create(collectionName, balance, Realm.UpdateMode.Never);
